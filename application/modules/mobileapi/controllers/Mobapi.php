d404a9938928d1d9b7be69fff28de2de3759f73c
khadkap <khadkap007@gmail.com>
2019-07-10 17:25:31 +0545

    nepali word

--- application/modules/mobileapi/controllers/Mobapi.php
+++ application/modules/mobileapi/controllers/Mobapi.php
@@ -33,8 +33,7 @@ class Mobapi extends Admin_Controller
     public function touristInformation()
     {
       $lang = $this->input->post('language');
-      $language=$lang['Language'];
-      $data=$this->general->get_tbl_data_result('*','touristinformation',array('language'=>$language));//this is language table
+      $data=$this->general->get_tbl_data_result('*','touristinformation',array('language'=>$lang));//this is language table
       $response['error'] = 0 ;
       $response['message'] = 'List Of Tourist Information';
       $response['data'] = $data;
