55b635ebc6466fe71a84045c2c798505b98dc79d
khadkap <khadkap007@gmail.com>
2019-10-04 15:28:49 +0545

    image size

--- application/modules/map/views/admin/manage_details.php
+++ application/modules/map/views/admin/manage_details.php
@@ -64,7 +64,8 @@
                             
                             <div id="content-to-print<?php echo $v['id'] ?>">
                                   Name : <?php echo $v['name'] ?><br>
-                                  <img src="<?php echo $v['primary_image'] ?>" height='210px'><br>
+                                  <?php $first = reset($v['iamges']); ?>
+                                  <img src="<?php echo $first ?>" height='210px'><br>
                                   <img src="<?php echo $v['qr_code'] ?>">
                               </div>
                             </td>
